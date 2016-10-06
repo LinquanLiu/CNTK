b244ccbbf7dcd436ca371bc0ce102875d17a7c40
Eldar Akchurin <eldak@microsoft.com>
2016-10-06 14:05:15 +0200

    Allowing deserializers to return invalid sequences

--- Source/Readers/ImageReader/ImageUtil.h
+++ Source/Readers/ImageReader/ImageUtil.h
@@ -6,8 +6,6 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
-#include "SequenceData.h"
-#include <numeric>
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
@@ -51,51 +49,4 @@ namespace Microsoft { namespace MSR { namespace CNTK {
         }
         return resultType;
     }
-
-    // A helper interface to generate a typed label in a sparse format for categories.
-    // It is represented as an array indexed by the category, containing zero values for all categories the sequence does not belong to,
-    // and a single one for a category it belongs to: [ 0 .. 0.. 1 .. 0 ]
-    class LabelGenerator
-    {
-    public:
-        virtual void CreateLabelFor(size_t classId, CategorySequenceData& data) = 0;
-        virtual size_t LabelDimension() const = 0;
-        virtual ~LabelGenerator() { }
-    };
-    typedef std::shared_ptr<LabelGenerator> LabelGeneratorPtr;
-
-    // Simple implementation of the LabelGenerator.
-    // The class is parameterized because the representation of 1 is type specific.
-    template <class TElement>
-    class TypedLabelGenerator : public LabelGenerator
-    {
-    public:
-        TypedLabelGenerator(size_t labelDimension) : m_value(1), m_indices(labelDimension)
-        {
-            if (labelDimension > numeric_limits<IndexType>::max())
-            {
-                RuntimeError("Label dimension (%d) exceeds the maximum allowed "
-                    "value (%d)\n", (int)labelDimension, (int)numeric_limits<IndexType>::max());
-            }
-            iota(m_indices.begin(), m_indices.end(), 0);
-        }
-
-        void CreateLabelFor(size_t classId, CategorySequenceData& data) override
-        {
-            data.m_nnzCounts.resize(1);
-            data.m_nnzCounts[0] = 1;
-            data.m_totalNnzCount = 1;
-            data.m_data = &m_value;
-            data.m_indices = &(m_indices[classId]);
-        }
-
-        size_t LabelDimension() const override
-        {
-            return m_indices.size();
-        }
-
-    private:
-        TElement m_value;
-        vector<IndexType> m_indices;
-    };
 }}}
